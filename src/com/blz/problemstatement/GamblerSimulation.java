f701796a867821f7a7d2c0f0f6b574349df3045a
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-10-27 00:56:18 +0530

    Added:Game Start With Initial StackValue

---
+++ src/com/blz/problemstatement/GamblerSimulation.java
@@ -0,0 +1,15 @@
+package com.blz.problemstatement;
+
+public class GamblerSimulation {
+	static int stake = 100;
+	static int bet = 1;
+
+	// UC1
+	public static void initialMoney() {
+		System.out.println("Stake_Amount:" + stake);
+	}
+
+	public static void main(String[] args) {
+		initialMoney();
+	}
+}
